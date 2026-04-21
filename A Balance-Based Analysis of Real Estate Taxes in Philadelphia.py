30aa2668da4d43a8c7cc5924229a86546e22032d
Mia <tuu83062@temple.edu>
2026-04-21 02:11:39 -0400

    Required imports 2

--- A Balance-Based Analysis of Real Estate Taxes in Philadelphia.py
+++ A Balance-Based Analysis of Real Estate Taxes in Philadelphia.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from rich import print
+#from rich import print
+
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
