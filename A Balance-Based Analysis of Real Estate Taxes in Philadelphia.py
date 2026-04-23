232caabf314f742011860a0d5b35830c4d88fafa
Mia <tuu83062@temple.edu>
2026-04-22 21:12:38 -0400

    contents of the data file, introduction

--- A Balance-Based Analysis of Real Estate Taxes in Philadelphia.py
+++ A Balance-Based Analysis of Real Estate Taxes in Philadelphia.py
@@ -8,12 +8,21 @@ from streamlit_folium import st_folium
 
 st.title("A Balance-Based Analysis of Real Estate Taxes in Philadelphia")
 
-st.write(...) # Add a description/introduction to the dashboard
+st.write("This project analyzes a dataset published on OpenDataPhilly, which summarizes delinquent real estate taxes in the city of Philadelphia (categorized by ZIP code). Each column is a piece of a larger picture, and together they show how tax debt varies across different areas.") #introduction
 
-my_map = folium.Map(location=[39.9526, -75.1652], zoom_start=12)
+my_map = folium.Map(location=[39.9526, -75.1652], zoom_start=12) #map centered on Philadelphia
 folium.Marker([39.9526, -75.1652], popup="Philadelphia").add_to(my_map)
 st_folium(my_map)
 
-dataFile=pd.read_csv("real_estate_tax_balances_zip_code.csv")
-st.subheader("File Contents")
+dataFile=pd.read_csv("real_estate_tax_balances_zip_code.csv") #load the data file
+st.subheader("File Contents") #display the contents of the data file, working as a summary
 st.dataframe(dataFile)
+
+dataFile.columns=dataFile.columns.str.strip().str.lower().str.replace(" ", "_")
+sumColumns=["num_props", "principal", "interest", "panalty", "other", "balance"]
+
+for col in sumColumns:
+    dataFile[col]=pd.to_numeric(dataFile[col], errors="coerce")
+dataFile=dataFile.dropna() #drop rows with missing values
+st.subheader("Summary Statistics")
+dataFile["avg_balance"]=dataFile["balance"]/dataFile["num_props"]
